4ecbc0c6daf297362cca980dfa7a69fa9d3596c8
Gabriel Leis <gabrielleis@Gabriels-MacBook-Pro.local>
2025-09-20 12:02:51 +0200

    Version 1.0.2 - Fixed minor issues

--- src/main/java/com/roundforma/website/springboot_roundforma_website/controllers/IndexController.java
+++ src/main/java/com/roundforma/website/springboot_roundforma_website/controllers/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainControllerImpl {
+public class IndexController {
     //Endpoint del home page
     @GetMapping("/inicio")
     public String getHomePage(){
--- src/main/java/com/roundforma/website/springboot_roundforma_website/controllers/MainController.java
+++
@@ -1,6 +0,0 @@
-package com.roundforma.website.springboot_roundforma_website.controllers;
-
-public interface MainController {
-    //Método que retorne la página home.
-    public String getHomePage();
-}
